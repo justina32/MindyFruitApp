1e18c508082e47d3f005fded887c165b47c2267f
manyuan.tong <manyuan.tong@bytedance.com>
2023-09-12 01:15:09 -0700

    compose

--- app/src/main/java/com/example/mindyfruitapplication2/MainActivity.kt
+++ app/src/main/java/com/example/mindyfruitapplication2/MainActivity.kt
@@ -1,32 +1,72 @@
 package com.example.mindyfruitapplication2
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mindyfruitapplication2.fragment.FruitTreeFragment
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mindyfruitapplication2.ui.theme.MindyFruitApplication2Theme
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class MainActivity : ComponentActivity() {
+    companion object {
+        const val BANANA_PATH = "api/fruit/banana"
+        val FRUIT_LIST = listOf("Apple", "Banana", "Orange","Persimmon","Strawberry","Pear","Durian"
+            ,"Blackberry","Lingonberry","Kiwi","Lychee","Pineapple","Fig","Gooseberry","Passionfruit",
+            "Plum","Orange","GreenApple","Raspberry","Watermelon")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MindyFruitApplication2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Fruit")
-                }
+                FruitFamilyTreeScreen(fruitList = FRUIT_LIST)
             }
         }
+    }
+
+
+fun initData(): View.OnClickListener? {
+    Thread {
+        getDataFromServer()
+    }.start()
+    return null
+}
+
+fun getDataFromServer() {
+    try {
+        // 创建一个 OkHttpClient 对象
+        val client = OkHttpClient()
+
+// 构建一个 HTTP 请求，指定请求 URL 为 "api/fruit/all"
+        val request = Request.Builder()
+            .url(BANANA_PATH)
+            .build()
+
+// 使用创建的 client 对象发送请求，并获取响应对象
+        val response = client.newCall(request).execute()
+
+// 从响应对象中获取响应数据（以字符串格式返回）
+        val responseData = response.body()?.string()
+        print(responseData)
+    } catch (e: Throwable) {
+        print("error")
+
     }
 }
 
@@ -46,5 +86,34 @@ fun GreetingPreview() {
     }
 }
 
-val myFragment = FruitTreeFragment()
+    @Composable
+    fun FruitFamilyTreeScreen(fruitList: List<String>) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text(
+                "Let's Eat Fruit",
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                fontSize = 35.sp,
+                modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
+            )
+
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                items(fruitList) { fruit ->
+                    Text(
+                        text = fruit,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Medium,
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(vertical = 8.dp, horizontal = 16.dp)
+                    )
+                }
+            }
+        }
+    }
+}
 
